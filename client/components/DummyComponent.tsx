43b9487252ca4244eca2742730f7e58e4b84c334
Tanzim Mokammel <tanzim.mokammel@diginex.com>
2019-02-11 19:45:10 +0100

    remove svg and related

--- client/components/DummyComponent.tsx
+++ client/components/DummyComponent.tsx
@@ -1,15 +1,12 @@
 import * as React from "react";
 import { Text, View } from "react-native";
-import Clock from "../assets/icons/Clock";
-import Data from "../assets/icons/Data";
 
 export default class DummyComponent extends React.Component {
   public dummyString: string = "dummyString";
   public render() {
     return (
       <View>
-        {/* <Text>{this.dummyString}</Text> */}
-        <Data height={40} width={40}/>
+        <Text>{this.dummyString}</Text>
       </View>
     );
   }
